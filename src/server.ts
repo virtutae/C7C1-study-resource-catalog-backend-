426127cb1f5a4ee97977ef94f505e32aa2354135
VDromaretska <viktoriya.dromaretska@gmail.com>
2023-09-14 15:29:24 +0200

    Added sql code for left join

--- src/server.ts
+++ src/server.ts
@@ -29,6 +29,25 @@ app.get("/users", async (_req, res) => {
         res.status(500).send("An error occurred. Check server logs.");
     }
 });
+// export interface Recommendation {
+//     url: string;
+//     name: string;
+//     author: string;
+//     description: string;
+//     content_type: string;
+//     build_phase: string;
+//     creation_date: Date; //verify specific type for this one
+//     user_id: number;
+//     recommendation_type:
+//         | "I recommend this resource after having used it"
+//         | "I do not recommend this resource, having used it"
+//         | "I haven't used this resosurce but it looks promising";
+//     reason: string;
+//     likes:number;
+//     dislikes:number;
+//     tags: string[];
+//     comments:UserComment[];
+// }
 
 app.get("/recommendation/recent10", async (_req, res) => {
     try {
